295d933d1aeb5b3816cae6002161a48448dd4a7f
Likai Liu <likailiu2@gmail.com>
2022-11-20 10:40:07 +0000

    Implement a recursive function that calculates the sum from a given number to another given number

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return fromN;
+  }
+  return sum(fromN, toN - 1) + toN;
 }
 
 module.exports = sum;
